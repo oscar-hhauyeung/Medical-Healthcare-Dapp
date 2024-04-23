e391a197cb52582f7022c351c7d03beee9295180
JohnnyTung123 <johnnya12399@gmail.com>
2024-04-23 22:32:17 +0800

    url fixed

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://medical-healthcare-dapp-1030.vercel.app/",
+      "https://medical-healthcare-dapp-1030.vercel.app",
       "http://localhost:3000",
     ],
     methods: ["GET", "POST"],
